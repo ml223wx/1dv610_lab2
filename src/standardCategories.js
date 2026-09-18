a45bf812ed44903311305f70cfed5034e8f22ea5
Miranda Lundin <ml223wx@student.lnu.se>
2026-09-18 12:27:13 +0200

    Enter standard categories

--- src/standardCategories.js
+++ src/standardCategories.js
@@ -1,8 +1,11 @@
 import { Category } from "./Categories.js"
 
-export const hobby = new Category("Hobby", ["Craft", "Vacation", "Events", "Subscriptions"])
+export const communication = new Category("Communication", ["Post", "Telephone"])
 export const food = new Category("Food", ["Groceries", "Restaurant", "Snacks"])
+export const health = new Category("Health & Beauty", ["Beauty", "Healthcare"])
+export const hobby = new Category("Hobby", ["Crafts", "Vacation", "Events", "Subscriptions"])
+export const household = new Category("Household", ["Consumables", "Decor"])
+export const shopping = new Category("Shopping", ["Clothes & accessories", "Stationaries"])
+export const transportation = new Category("Transportation", ["Long distance", "Public transport"])
 
-export const categories = [hobby, food]
-
-export const testVar = "detta är en testvariabel"
+export const categories = [communication, food, health, hobby, household, shopping, transportation]
